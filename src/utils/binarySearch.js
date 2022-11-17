8a828256bbd93aaeb3f2f7ac5434b7d26b0c1430
Cody Barr <cody.barr@gmail.com>
2022-11-17 07:44:39 -0800

    refactor: move binarySearch to separate file

---
+++ src/utils/binarySearch.js
@@ -0,0 +1,22 @@
+// find binary search closest match.
+// Source: https://stackoverflow.com/a/48876834 .
+export default (arr, target, lo = 0, hi = arr.length - 1) => {
+  if (target < arr[lo]) {
+    return arr[0]
+  }
+  if (target > arr[hi]) {
+    return arr[hi]
+  }
+
+  const mid = Math.floor((hi + lo) / 2)
+
+  return hi - lo < 2
+    ? target - arr[lo] < arr[hi] - target
+      ? arr[lo]
+      : arr[hi]
+    : target < arr[mid]
+    ? binarySearch(arr, target, lo, mid)
+    : target > arr[mid]
+    ? binarySearch(arr, target, mid, hi)
+    : arr[mid]
+}
